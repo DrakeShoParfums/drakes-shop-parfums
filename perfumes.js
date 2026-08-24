4f49e9bac9cb742ef0f31337b58e55a8aece9f60
DrakeShoParfums <rottibrawlstars@gmail.com>
2026-08-24 17:37:01 -0600

    Add initial perfumes data to perfumes.js

---
+++ perfumes.js
@@ -0,0 +1,123 @@
+const perfumes = [
+
+  {
+    nombre: "Dior Sauvage Eau de Parfum",
+    marca: "Dior",
+    ml: 100,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "Aromática",
+    acordes: [
+      "Fresco",
+      "Amaderado",
+      "Especiado"
+    ],
+    notas: [
+      "Bergamota",
+      "Pimienta",
+      "Lavanda"
+    ],
+    imagen: "imagenes/dior-sauvage-edp.jpg"
+  },
+
+  {
+    nombre: "Versace Eros",
+    marca: "Versace",
+    ml: 100,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "Aromática",
+    acordes: [
+      "Dulce",
+      "Fresco",
+      "Amaderado"
+    ],
+    notas: [
+      "Menta",
+      "Manzana",
+      "Vainilla"
+    ],
+    imagen: "imagenes/versace-eros.jpg"
+  },
+
+  {
+    nombre: "Jean Paul Gaultier Le Male",
+    marca: "Jean Paul Gaultier",
+    ml: 125,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "Oriental",
+    acordes: [
+      "Dulce",
+      "Aromático",
+      "Vainilla"
+    ],
+    notas: [
+      "Lavanda",
+      "Menta",
+      "Vainilla"
+    ],
+    imagen: "imagenes/jpg-le-male.jpg"
+  },
+
+  {
+    nombre: "Lattafa Khamrah",
+    marca: "Lattafa",
+    ml: 100,
+    genero: "Unisex",
+    tipo: "Eau de Parfum",
+    familia: "Oriental",
+    acordes: [
+      "Dulce",
+      "Especiado",
+      "Vainilla"
+    ],
+    notas: [
+      "Canela",
+      "Dátiles",
+      "Vainilla"
+    ],
+    imagen: "imagenes/lattafa-khamrah.jpg"
+  },
+
+  {
+    nombre: "Carolina Herrera 212 VIP",
+    marca: "Carolina Herrera",
+    ml: 80,
+    genero: "Mujer",
+    tipo: "Eau de Parfum",
+    familia: "Floral",
+    acordes: [
+      "Dulce",
+      "Floral",
+      "Amaderado"
+    ],
+    notas: [
+      "Maracuyá",
+      "Gardenia",
+      "Vainilla"
+    ],
+    imagen: "imagenes/212-vip.jpg"
+  },
+
+  {
+    nombre: "Acqua di Gio",
+    marca: "Giorgio Armani",
+    ml: 100,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "Acuática",
+    acordes: [
+      "Acuático",
+      "Cítrico",
+      "Fresco"
+    ],
+    notas: [
+      "Limón",
+      "Jazmín",
+      "Cedro"
+    ],
+    imagen: "imagenes/acqua-di-gio.jpg"
+  }
+
+];
