41a48ee41c7b95e7be9fb6b88aaad2659fd9cdf1
DrakeShoParfums <rottibrawlstars@gmail.com>
2026-08-24 18:13:47 -0600

    Update perfume entries for Jean Paul Gaultier

--- perfumes.js
+++ perfumes.js
@@ -1,123 +1,808 @@
 const perfumes = [
 
+  // ============================
+  // JEAN PAUL GAULTIER
+  // ============================
+
   {
-    nombre: "Dior Sauvage Eau de Parfum",
-    marca: "Dior",
-    ml: 100,
+    nombre: "Le Male Elixir",
+    marca: "Jean Paul Gaultier",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/jpg-le-male-elixir.jpg"
+  },
+
+  {
+    nombre: "Le Male Absolu",
+    marca: "Jean Paul Gaultier",
+    ml: null,
+    genero: "Hombre",
+    tipo: "",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/jpg-le-male-absolu.jpg"
+  },
+
+  {
+    nombre: "Le Beau Paradise Garden",
+    marca: "Jean Paul Gaultier",
+    ml: null,
     genero: "Hombre",
     tipo: "Eau de Parfum",
-    familia: "Aromática",
-    acordes: [
-      "Fresco",
-      "Amaderado",
-      "Especiado"
-    ],
-    notas: [
-      "Bergamota",
-      "Pimienta",
-      "Lavanda"
-    ],
-    imagen: "imagenes/dior-sauvage-edp.jpg"
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/jpg-le-beau-paradise-garden.jpg"
   },
 
   {
-    nombre: "Versace Eros",
-    marca: "Versace",
-    ml: 100,
-    genero: "Hombre",
-    tipo: "Eau de Toilette",
-    familia: "Aromática",
-    acordes: [
-      "Dulce",
-      "Fresco",
-      "Amaderado"
-    ],
-    notas: [
-      "Menta",
-      "Manzana",
-      "Vainilla"
-    ],
-    imagen: "imagenes/versace-eros.jpg"
-  },
-
-  {
-    nombre: "Jean Paul Gaultier Le Male",
+    nombre: "Le Male Le Parfum",
     marca: "Jean Paul Gaultier",
-    ml: 125,
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/jpg-le-male-le-parfum.jpg"
+  },
+
+  {
+    nombre: "Le Beau Le Parfum",
+    marca: "Jean Paul Gaultier",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/jpg-le-beau-le-parfum.jpg"
+  },
+
+  {
+    nombre: "Le Beau",
+    marca: "Jean Paul Gaultier",
+    ml: null,
     genero: "Hombre",
     tipo: "Eau de Toilette",
-    familia: "Oriental",
-    acordes: [
-      "Dulce",
-      "Aromático",
-      "Vainilla"
-    ],
-    notas: [
-      "Lavanda",
-      "Menta",
-      "Vainilla"
-    ],
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/jpg-le-beau.jpg"
+  },
+
+  {
+    nombre: "Ultra Male",
+    marca: "Jean Paul Gaultier",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/jpg-ultra-male.jpg"
+  },
+
+  {
+    nombre: "Le Male",
+    marca: "Jean Paul Gaultier",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
     imagen: "imagenes/jpg-le-male.jpg"
   },
 
   {
-    nombre: "Lattafa Khamrah",
-    marca: "Lattafa",
-    ml: 100,
-    genero: "Unisex",
-    tipo: "Eau de Parfum",
-    familia: "Oriental",
-    acordes: [
-      "Dulce",
-      "Especiado",
-      "Vainilla"
-    ],
-    notas: [
-      "Canela",
-      "Dátiles",
-      "Vainilla"
-    ],
-    imagen: "imagenes/lattafa-khamrah.jpg"
+    nombre: "Scandal Absolu",
+    marca: "Jean Paul Gaultier",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/jpg-scandal-absolu.jpg"
   },
 
   {
-    nombre: "Carolina Herrera 212 VIP",
-    marca: "Carolina Herrera",
-    ml: 80,
-    genero: "Mujer",
+    nombre: "Scandal Intense",
+    marca: "Jean Paul Gaultier",
+    ml: null,
+    genero: "Hombre",
     tipo: "Eau de Parfum",
-    familia: "Floral",
-    acordes: [
-      "Dulce",
-      "Floral",
-      "Amaderado"
-    ],
-    notas: [
-      "Maracuyá",
-      "Gardenia",
-      "Vainilla"
-    ],
-    imagen: "imagenes/212-vip.jpg"
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/jpg-scandal-intense.jpg"
   },
 
   {
-    nombre: "Acqua di Gio",
-    marca: "Giorgio Armani",
-    ml: 100,
+    nombre: "Scandal Pour Homme Le Parfum",
+    marca: "Jean Paul Gaultier",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/jpg-scandal-pour-homme-le-parfum.jpg"
+  },
+
+  {
+    nombre: "Scandal Pour Homme",
+    marca: "Jean Paul Gaultier",
+    ml: null,
     genero: "Hombre",
     tipo: "Eau de Toilette",
-    familia: "Acuática",
-    acordes: [
-      "Acuático",
-      "Cítrico",
-      "Fresco"
-    ],
-    notas: [
-      "Limón",
-      "Jazmín",
-      "Cedro"
-    ],
-    imagen: "imagenes/acqua-di-gio.jpg"
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/jpg-scandal-pour-homme.jpg"
+  },
+
+
+  // ============================
+  // VERSACE
+  // ============================
+
+  {
+    nombre: "Eros Parfum",
+    marca: "Versace",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/versace-eros-parfum.jpg"
+  },
+
+  {
+    nombre: "Eros Flame",
+    marca: "Versace",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/versace-eros-flame.jpg"
+  },
+
+  {
+    nombre: "Eros",
+    marca: "Versace",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/versace-eros-edp.jpg"
+  },
+
+  {
+    nombre: "Eros",
+    marca: "Versace",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/versace-eros-edt.jpg"
+  },
+
+  {
+    nombre: "Dylan",
+    marca: "Versace",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/versace-dylan.jpg"
+  },
+
+  {
+    nombre: "Eros Najim",
+    marca: "Versace",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/versace-eros-najim.jpg"
+  },
+
+  {
+    nombre: "Eros Energy",
+    marca: "Versace",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/versace-eros-energy.jpg"
+  },
+
+
+  // ============================
+  // PACO RABANNE / RABANNE
+  // ============================
+
+  {
+    nombre: "Invictus Parfum",
+    marca: "Rabanne",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/rabanne-invictus-parfum.jpg"
+  },
+
+  {
+    nombre: "Invictus Victory",
+    marca: "Rabanne",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/rabanne-invictus-victory.jpg"
+  },
+
+  {
+    nombre: "Invictus Victory Extreme",
+    marca: "Rabanne",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/rabanne-invictus-victory-extreme.jpg"
+  },
+
+  {
+    nombre: "Invictus Victory Elixir",
+    marca: "Rabanne",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/rabanne-invictus-victory-elixir.jpg"
+  },
+
+  {
+    nombre: "Invictus",
+    marca: "Rabanne",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/rabanne-invictus.jpg"
+  },
+
+  {
+    nombre: "Phantom Intense",
+    marca: "Rabanne",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/rabanne-phantom-intense.jpg"
+  },
+
+  {
+    nombre: "Phantom",
+    marca: "Rabanne",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/rabanne-phantom.jpg"
+  },
+
+  {
+    nombre: "Phantom Parfum",
+    marca: "Rabanne",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/rabanne-phantom-parfum.jpg"
+  },
+
+  {
+    nombre: "Phantom Elixir",
+    marca: "Rabanne",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/rabanne-phantom-elixir.jpg"
+  },
+
+
+  // ============================
+  // CAROLINA HERRERA
+  // ============================
+
+  {
+    nombre: "Bad Boy Extreme",
+    marca: "Carolina Herrera",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/ch-bad-boy-extreme.jpg"
+  },
+
+  {
+    nombre: "Bad Boy Le Parfum",
+    marca: "Carolina Herrera",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/ch-bad-boy-le-parfum.jpg"
+  },
+
+  {
+    nombre: "Bad Boy",
+    marca: "Carolina Herrera",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/ch-bad-boy.jpg"
+  },
+
+  {
+    nombre: "Bad Boy Cobalt Elixir",
+    marca: "Carolina Herrera",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/ch-bad-boy-cobalt-elixir.jpg"
+  },
+
+  {
+    nombre: "Bad Boy Elixir",
+    marca: "Carolina Herrera",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/ch-bad-boy-elixir.jpg"
+  },
+
+  {
+    nombre: "Bad Boy Sparkling Ice",
+    marca: "Carolina Herrera",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/ch-bad-boy-sparkling-ice.jpg"
+  },
+
+  {
+    nombre: "Bad Boy Cobalt",
+    marca: "Carolina Herrera",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/ch-bad-boy-cobalt.jpg"
+  },
+
+  {
+    nombre: "212 VIP Black",
+    marca: "Carolina Herrera",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/ch-212-vip-black.jpg"
+  },
+
+  {
+    nombre: "212 VIP",
+    marca: "Carolina Herrera",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/ch-212-vip.jpg"
+  },
+
+  {
+    nombre: "212 Heroes",
+    marca: "Carolina Herrera",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/ch-212-heroes.jpg"
+  },
+
+  {
+    nombre: "212 VIP Black Elixir",
+    marca: "Carolina Herrera",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/ch-212-vip-black-elixir.jpg"
+  },
+
+
+  // ============================
+  // CREED
+  // ============================
+
+  {
+    nombre: "Aventus",
+    marca: "Creed",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/creed-aventus.jpg"
+  },
+
+  {
+    nombre: "Millesime Imperial",
+    marca: "Creed",
+    ml: null,
+    genero: "Unisex",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/creed-millesime-imperial.jpg"
+  },
+
+  {
+    nombre: "Absolu Aventus",
+    marca: "Creed",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/creed-absolu-aventus.jpg"
+  },
+
+  {
+    nombre: "Himalaya",
+    marca: "Creed",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/creed-himalaya.jpg"
+  },
+
+  {
+    nombre: "Green Irish Tweed",
+    marca: "Creed",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/creed-green-irish-tweed.jpg"
+  },
+
+  {
+    nombre: "Erolfa",
+    marca: "Creed",
+    ml: null,
+    genero: "Unisex",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/creed-erolfa.jpg"
+  },
+
+
+  // ============================
+  // LE LABO
+  // ============================
+
+  {
+    nombre: "Santal 33",
+    marca: "Le Labo",
+    ml: null,
+    genero: "Unisex",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/le-labo-santal-33.jpg"
+  },
+
+
+  // ============================
+  // MOSCHINO
+  // ============================
+
+  {
+    nombre: "Toy Boy",
+    marca: "Moschino",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/moschino-toy-boy.jpg"
+  },
+
+  {
+    nombre: "Toy Boy 2",
+    marca: "Moschino",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/moschino-toy-boy-2.jpg"
+  },
+
+
+  // ============================
+  // GIORGIO ARMANI
+  // ============================
+
+  {
+    nombre: "Acqua di Giò Profondo",
+    marca: "Giorgio Armani",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/acqua-di-gio-profondo-parfum.jpg"
+  },
+
+  {
+    nombre: "Acqua di Giò Profondo",
+    marca: "Giorgio Armani",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/acqua-di-gio-profondo-edp.jpg"
+  },
+
+  {
+    nombre: "Acqua di Giò",
+    marca: "Giorgio Armani",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/acqua-di-gio-parfum.jpg"
+  },
+
+  {
+    nombre: "Acqua di Giò",
+    marca: "Giorgio Armani",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/acqua-di-gio-edp.jpg"
+  },
+
+
+  // ============================
+  // DIOR
+  // ============================
+
+  {
+    nombre: "Sauvage Parfum",
+    marca: "Dior",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/dior-sauvage-parfum.jpg"
+  },
+
+  {
+    nombre: "Sauvage Elixir",
+    marca: "Dior",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/dior-sauvage-elixir.jpg"
+  },
+
+  {
+    nombre: "Sauvage",
+    marca: "Dior",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/dior-sauvage-edp.jpg"
+  },
+
+  {
+    nombre: "Sauvage Eau Forte",
+    marca: "Dior",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/dior-sauvage-eau-forte.jpg"
+  },
+
+  {
+    nombre: "Sauvage",
+    marca: "Dior",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/dior-sauvage-edt.jpg"
+  },
+
+
+  // ============================
+  // VALENTINO
+  // ============================
+
+  {
+    nombre: "Born in Roma Intense Uomo",
+    marca: "Valentino",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/valentino-born-in-roma-intense-uomo.jpg"
+  },
+
+  {
+    nombre: "Born in Roma Extradose Uomo",
+    marca: "Valentino",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/valentino-born-in-roma-extradose-uomo.jpg"
+  },
+
+  {
+    nombre: "Born in Roma Uomo",
+    marca: "Valentino",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/valentino-born-in-roma-uomo.jpg"
+  },
+
+  {
+    nombre: "Uomo Intense",
+    marca: "Valentino",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Parfum",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/valentino-uomo-intense.jpg"
+  },
+
+  {
+    nombre: "Born in Roma Coral Fantasy",
+    marca: "Valentino",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/valentino-born-in-roma-coral-fantasy.jpg"
+  },
+
+  {
+    nombre: "Born in Roma Green Stravaganza",
+    marca: "Valentino",
+    ml: null,
+    genero: "Hombre",
+    tipo: "Eau de Toilette",
+    familia: "",
+    acordes: [],
+    notas: [],
+    imagen: "imagenes/valentino-born-in-roma-green-stravaganza.jpg"
   }
 
 ];
